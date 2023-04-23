491548f7b480b120d7cdd48f40ba14bad4134ae9
nmatai <nmatai@usc.edu>
2023-04-23 07:52:07 -0700

    deployed to firebase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,23 +6,37 @@ import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
-
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const firebaseConfig = {
+  apiKey: "AIzaSyCInRYTChLDjV_mQIiextqIFHx7nspmJNE",
+  authDomain: "carbon-counter-a38b9.firebaseapp.com",
+  projectId: "carbon-counter-a38b9",
+  storageBucket: "carbon-counter-a38b9.appspot.com",
+  messagingSenderId: "436940481490",
+  appId: "1:436940481490:web:87031217c899038fe49e0b",
+  measurementId: "G-86E9SZFVKH",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 root.render(
   <Auth0Provider
     domain="dev-7n6cr0svckgxjyod.us.auth0.com"
     clientId="kPsIXVkUBkwis1GPnBeIQi8RCIQLLW5z"
-    
     authorizationParams={{
-      redirect_uri:"http://localhost:3000/loader"
+      redirect_uri: window.location.origin+"/loader",
     }}
   >
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    {/* {console.log(window.location.origin)} */}
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
   </Auth0Provider>
 );
 
